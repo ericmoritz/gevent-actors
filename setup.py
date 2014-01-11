41a71ec3841a36a1cc79bbe9d8d21e14daa2c43c
Eric Moritz <eric@themoritzfamily.com>
2014-01-11 01:52:02 -0500

    initial version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(
     name="gevent-actors",
+    version="0.1",
     description="basic actors using gevent and kombu for communication",
     modules=['actors'],
     install_requires=[
