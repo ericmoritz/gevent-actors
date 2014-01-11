1475a068c25f29bfb527f0e34955cadee6907697
Eric Moritz <eric@themoritzfamily.com>
2014-01-11 02:20:16 -0500

    made actor into a package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="gevent-actors",
     version="0.1.1",
     description="basic actors using gevent and kombu for communication",
-    modules=['actor'],
+    packages=['actor'],
     install_requires=[
         "kombu",
         "gevent",
